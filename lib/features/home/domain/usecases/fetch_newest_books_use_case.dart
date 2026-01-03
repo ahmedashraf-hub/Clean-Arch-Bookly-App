b6b2dd8ff58ac548c1d56b0d43af2bea221b5650
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2026-01-03 17:13:03 +0200

    generic use case for two useCases fetch fetured and fetch newest

--- lib/features/home/domain/usecases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/usecases/fetch_newest_books_use_case.dart
@@ -3,14 +3,14 @@ import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
-  FetchFeaturedBooksUseCase(this.homeRepo);
+  FetchNewestBooksUseCase(this.homeRepo);
 
   @override
   Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
-    return await homeRepo.fetchFeaturedBooks();
+    return await homeRepo.fetchNewestBooks();
   }
 }
 
