53707d0683842c812986740002da337419f2dc42
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2026-01-05 00:33:06 +0200

    book model extends book entity

--- lib/features/home/data/models/book_model/book_model.dart
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -1,9 +1,11 @@
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+
 import 'access_info.dart';
 import 'sale_info.dart';
 import 'search_info.dart';
 import 'volume_info.dart';
 
-class BookModel {
+class BookModel extends BookEntity {
   String? kind;
   String? id;
   String? etag;
@@ -22,7 +24,14 @@ class BookModel {
     this.saleInfo,
     this.accessInfo,
     this.searchInfo,
-  });
+  }) : super(
+         bookId: id!,
+         image: volumeInfo?.imageLinks?.thumbnail ?? '',
+         authorName: volumeInfo?.authors?.first ?? 'No Name',
+         price: 0.0,
+         rating: volumeInfo!.averageRating,
+         title: volumeInfo.title!,
+       );
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
     kind: json['kind'] as String?,
@@ -45,7 +54,7 @@ class BookModel {
 
   Map<String, dynamic> toJson() => {
     'kind': kind,
-    'id': id,
+    'id': bookId,
     'etag': etag,
     'selfLink': selfLink,
     'volumeInfo': volumeInfo?.toJson(),
